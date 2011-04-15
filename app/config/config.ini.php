ee928dfd2820f0a68057fed1f2df99a6a5c7aaac
Sergey Lukin <smartcoding@live.com>
2011-04-15 22:57:05 +0300

    =added empty .gitignore files to empty directories

---
+++ app/config/config.ini.php
@@ -0,0 +1,16 @@
+[system]
+default_controller = defaultController
+default_layout = default
+default_content = index
+error_controller = errorController
+error_layout = error
+error_content = error
+timezone = "America/Los_Angeles"
+
+[database]
+db_type = mysql
+db_name = test
+db_hostname = localhost
+db_username = test
+db_password = test
+db_port = 3306

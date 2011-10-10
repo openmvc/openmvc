d59d34c5718277a3ea2f1bb582b28bd973adb3c2
Sergey Lukin <smartcoding@live.com>
2011-10-10 00:10:08 +0000

    changed core reference to remote repo

--- public/index.php
+++ public/index.php
@@ -1,4 +1,21 @@
 <?php
 
-// Bootstrap the app
-require_once '../openmvc/app/bootstrap.php';
+/**
+ * Define "/" (UNIX) or "\" (WINDOWS) as DS 
+ */
+define('DS', DIRECTORY_SEPARATOR);
+
+/**
+ * Define Framework path
+ */
+define('OPENMVC_PATH', realpath(__DIR__ . '../openmvc') . DS);
+
+/**
+ * Define Application path 
+ */
+define('APP_PATH', realpath(__DIR__ . '../app') . DS);
+
+/**
+ * Bootstrap the application 
+ */
+require APP_PATH . 'bootstrap.php';
